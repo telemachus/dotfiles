21b6b4c7ed14ffa59efb12eba226a96605d6d3d3
Peter Aronoff <telemachus@arpinum.org>
2013-05-07 09:01:15 -0400

    Check hg completion too

--- bash-completion-updater.sh
+++ bash-completion-updater.sh
@@ -12,7 +12,10 @@ NEWB="/usr/local/etc/bash_completion.d/git-completion.bash"
 OLDP="$HOME/.bash_completion.d/git-prompt.bash"
 NEWP="/usr/local/etc/bash_completion.d/git-prompt.sh"
 
-for item in $OLDB $NEWB $OLDP $NEWP
+OLDHG="$HOME/.bash_completion.d/hg-completion.bash"
+NEWHG="/usr/local/etc/bash_completion.d/hg-completion.bash"
+
+for item in $OLDB $NEWB $OLDP $NEWP $OLDHG $NEWHG
 do
     if [ ! -f "$item" ]; then
         printf "%s doesn't exist. It should. Aborting.\n" $item
@@ -43,5 +46,18 @@ else
         : # Do nothing
     else
         cp -v "$NEWP" "$OLDP"
+    fi
+fi
+
+if diff -q "$OLDHG" "$NEWHG" >/dev/null 2>&1; then
+    printf "Identical:\n\t%s\n\t%s\n" $OLDHG $NEWHG
+else
+    printf "Updated:\n\t%s\n" "$NEWHG"
+    printf "Do you want to update it? (Y/n) "
+    read answer
+    if is_no "$answer"; then
+        : # Do nothing
+    else
+        cp -v "$NEWHG" "$OLDHG"
     fi
 fi
