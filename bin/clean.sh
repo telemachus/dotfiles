b8579f432948c4f3947069dc6199192d42c57a87
Peter Aronoff <telemachus@arpinum.org>
2013-05-30 10:27:42 -0400

    Add -f flag to chmod

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,8 +1,8 @@
 #!/usr/local/bin/dash
 
-chmod -N /Users/achilles/Public
-chmod -N /Users/achilles/Movies
-chmod -N /Users/achilles/Music
+chmod -fN /Users/achilles/Public
+chmod -fN /Users/achilles/Movies
+chmod -fN /Users/achilles/Music
 rm -rf /Users/achilles/Public
 rm -rf /Users/achilles/Movies
 rm -rf /Users/achilles/Music
