664aae16d6a56c59ea475b8f279a75eed74ddacd
Peter Aronoff <telemachus@arpinum.org>
2013-09-08 09:00:22 -0400

    Use my own statusline

--- pathogen_kickstart.sh
+++ pathogen_kickstart.sh
@@ -24,7 +24,6 @@ git clone https://github.com/telemachus/snipmate-snippets-bib.git
 git clone https://github.com/sjl/strftimedammit.vim.git
 git clone https://github.com/tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone https://github.com/bling/vim-airline.git
 git clone https://github.com/PeterRincker/vim-argumentative.git
 git clone https://github.com/tpope/vim-commentary
 git clone https://github.com/tpope/vim-endwise.git
