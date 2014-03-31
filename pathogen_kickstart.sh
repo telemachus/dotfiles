c30efe68e77f9889e0d5f8e8f1bb97472f0b2066
Peter Aronoff <telemachus@arpinum.org>
2014-03-31 18:06:47 -0400

    Scale way, way down

--- pathogen_kickstart.sh
+++ pathogen_kickstart.sh
@@ -15,48 +15,8 @@ cd $HOME/.vim/autoload
 curl -so pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 cd $HOME/.vim/bundle
-git clone https://github.com/gregsexton/MatchTag.git
-git clone https://github.com/mileszs/ack.vim.git
-git clone https://github.com/jiangmiao/auto-pairs.git
-git clone https://github.com/vim-scripts/bufexplorer.zip.git
 git clone https://github.com/mattn/gist-vim.git
-git clone https://github.com/telemachus/snipmate-snippets-bib.git
-git clone https://github.com/sjl/strftimedammit.vim.git
-git clone https://github.com/tomtom/tlib_vim.git
-git clone https://github.com/Shougo/unite.vim.git
-git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone https://github.com/PeterRincker/vim-argumentative.git
-git clone https://github.com/tpope/vim-commentary
-git clone https://github.com/tpope/vim-endwise.git
-git clone https://github.com/tpope/vim-eunuch
-git clone https://github.com/terryma/vim-expand-region
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/tpope/vim-git.git
-git clone http://github.com/sukima/vim-markdown.git
-git clone https://github.com/kurkale6ka/vim-pairs.git
-git clone http://github.com/tpope/vim-rake.git
-git clone https://github.com/tpope/vim-repeat.git
-git clone https://github.com/vim-ruby/vim-ruby.git
-git clone https://github.com/sunaku/vim-ruby-minitest
-git clone https://github.com/tpope/vim-scriptease.git
-git clone https://github.com/garbas/vim-snipmate.git
-git clone https://github.com/telemachus/vim-snippets.git
+git clone https://github.com/plasticboy/vim-markdown.git
 git clone https://github.com/Pychimp/vim-sol.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/kana/vim-textobj-entire.git
-git clone https://github.com/kana/vim-textobj-function.git
-git clone https://github.com/kana/vim-textobj-indent
-git clone https://github.com/kana/vim-textobj-lastpat.git
-git clone https://github.com/kana/vim-textobj-line.git
-git clone https://github.com/nelstrom/vim-textobj-rubyblock.git
-git clone https://github.com/kana/vim-textobj-underscore.git
-git clone https://github.com/kana/vim-textobj-user.git
-git clone https://github.com/chriskempson/vim-tomorrow-theme.git
-git clone https://github.com/bronson/vim-trailing-whitespace.git
-git clone https://github.com/tpope/vim-unimpaired
 git clone https://github.com/telemachus/vim-varia.git
 git clone https://github.com/mattn/webapi-vim.git
-git clone https://github.com/wincent/Command-T.git &&
-    cd $HOME/.vim/bundle/Command-T/ruby/command-t &&
-    $(which ruby) extconf.rb &&
-    make
