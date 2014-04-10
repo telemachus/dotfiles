391ca779e77322fa1cb5bb229900857a2013401d
Peter Aronoff <telemachus@arpinum.org>
2014-04-10 08:48:33 -0400

    Shallow clones

--- pathogen_kickstart.sh
+++ pathogen_kickstart.sh
@@ -11,12 +11,16 @@ mkdir $HOME/.vim
 mkdir $HOME/.vim/autoload
 mkdir $HOME/.vim/bundle
 
+CLONE='git clone --depth=1'
+
 cd $HOME/.vim/autoload
 curl -so pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 cd $HOME/.vim/bundle
-git clone https://github.com/mattn/gist-vim.git
-git clone https://github.com/plasticboy/vim-markdown.git
-git clone https://github.com/Pychimp/vim-sol.git
-git clone https://github.com/telemachus/vim-varia.git
-git clone https://github.com/mattn/webapi-vim.git
+$CLONE https://github.com/mattn/gist-vim.git
+$CLONE https://github.com/plasticboy/vim-markdown.git
+$CLONE https://github.com/Pychimp/vim-sol.git
+$CLONE https://github.com/telemachus/vim-varia.git
+$CLONE https://github.com/mattn/webapi-vim.git
+$CLONE https://github.com/tpope/vim-surround.git
+$CLONE https://github.com/tpope/vim-repeat.git
