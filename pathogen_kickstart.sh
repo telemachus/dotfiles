49e651721fe6eabf6f1ff342ee8a057e3a183d46
Peter Aronoff <telemachus@arpinum.org>
2014-09-01 15:04:45 -0400

    Fix pathogen link

--- pathogen_kickstart.sh
+++ pathogen_kickstart.sh
@@ -7,18 +7,19 @@ if [ -e $HOME/.vim ]; then
     mv $HOME/.vim $HOME/old-vim
 fi
 
-mkdir -p $HOME/.vim/{autoload,bundle}
+mkdir -p $HOME/.vim/autoload
+mkdir -p $HOME/.vim/bundle
 
 CLONE='git clone --depth=1'
 
 cd $HOME/.vim/autoload
 curl -so pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd $HOME/.vim/bundle
-"$CLONE" https://github.com/mattn/gist-vim.git
-"$CLONE" https://github.com/SirVer/ultisnips.git
-"$CLONE" https://github.com/plasticboy/vim-markdown.git
-"$CLONE" https://github.com/tpope/vim-repeat.git
-"$CLONE" https://github.com/tpope/vim-surround.git
-"$CLONE" https://bitbucket.org/telemachus/vim-varia.git
-"$CLONE" https://github.com/mattn/webapi-vim.git
+$CLONE https://github.com/mattn/gist-vim.git
+$CLONE https://github.com/SirVer/ultisnips.git
+$CLONE https://github.com/plasticboy/vim-markdown.git
+$CLONE https://github.com/tpope/vim-repeat.git
+$CLONE https://github.com/tpope/vim-surround.git
+$CLONE https://bitbucket.org/telemachus/vim-varia.git
+$CLONE https://github.com/mattn/webapi-vim.git
