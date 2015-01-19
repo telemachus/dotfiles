fe62194f73beef85e13f5acb4acdc0467ae88761
Peter Aronoff <telemachus@arpinum.org>
2015-01-18 19:25:20 -0500

    Tweaking

---
+++ nvim-kickstart.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env dash
+
+set -e
+
+if [ -e $HOME/.nvim ]; then
+    printf "Backing up current .nvim directory to $HOME/old-nvim...\n"
+    mv $HOME/.nvim $HOME/old-nvim
+fi
+
+mkdir $HOME/.nvim
+mkdir $HOME/.nvim/autoload
+mkdir $HOME/.nvim/bundle
+
+CLONE='git clone'
+DEPTH='--depth 1'
+
+cd $HOME/.nvim/autoload
+curl -S -o pathogen.vim \
+    https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+cd $HOME/.nvim/bundle
+$CLONE $DEPTH https://github.com/mattn/gist-vim.git
+$CLONE $DEPTH https://github.com/nice/sweater.git
+$CLONE $DEPTH https://github.com/SirVer/ultisnips.git
+$CLONE $DEPTH https://github.com/markcornick/vim-bats.git
+$CLONE $DEPTH https://github.com/tpope/vim-markdown.git
+$CLONE $DEPTH https://github.com/tpope/vim-repeat.git
+$CLONE $DEPTH https://github.com/Pychimp/vim-sol.git
+$CLONE $DEPTH https://github.com/tpope/vim-surround.git
+$CLONE $DEPTH https://bitbucket.org/telemachus/vim-varia.git
+$CLONE $DEPTH https://github.com/mattn/webapi-vim.git
