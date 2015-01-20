7448dcbdf31000da25d1d2cad8ecb331f0d117be
Peter Aronoff <telemachus@arpinum.org>
2015-01-20 10:28:45 -0500

    Better bundles

--- nvim-kickstart.sh
+++ nvim-kickstart.sh
@@ -21,14 +21,14 @@ curl -S -o pathogen.vim \
     https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd $HOME/.nvim/bundle
-$CLONE $DEPTH https://github.com/mattn/gist-vim.git
-$CLONE $DEPTH https://github.com/nice/sweater.git
+$CLONE $DEPTH https://bitbucket.org/telemachus/vim-varia.git
+$CLONE $DEPTH https://github.com/Pychimp/vim-sol.git
 $CLONE $DEPTH https://github.com/SirVer/ultisnips.git
+$CLONE $DEPTH https://github.com/dietsche/vim-lastplace.git
 $CLONE $DEPTH https://github.com/markcornick/vim-bats.git
+$CLONE $DEPTH https://github.com/mattn/gist-vim.git
+$CLONE $DEPTH https://github.com/mattn/webapi-vim.git
+$CLONE $DEPTH https://github.com/nice/sweater.git
 $CLONE $DEPTH https://github.com/tpope/vim-markdown.git
 $CLONE $DEPTH https://github.com/tpope/vim-repeat.git
-$CLONE $DEPTH https://github.com/Pychimp/vim-sol.git
 $CLONE $DEPTH https://github.com/tpope/vim-surround.git
-$CLONE $DEPTH https://bitbucket.org/telemachus/vim-varia.git
-$CLONE $DEPTH https://github.com/mattn/webapi-vim.git
-$CLONE $DEPTH https://github.com/dietsche/vim-lastplace.git
--- vim-kickstart.sh
+++ vim-kickstart.sh
@@ -21,12 +21,13 @@ curl -S -o pathogen.vim \
     https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd $HOME/.vim/bundle
+$CLONE $DEPTH https://bitbucket.org/telemachus/vim-varia.git
+$CLONE $DEPTH https://github.com/Pychimp/vim-sol.git
 $CLONE $DEPTH https://github.com/SirVer/ultisnips.git
+$CLONE $DEPTH https://github.com/dietsche/vim-lastplace.git
 $CLONE $DEPTH https://github.com/mattn/gist-vim.git
 $CLONE $DEPTH https://github.com/mattn/webapi-vim.git
+$CLONE $DEPTH https://github.com/nice/sweater.git
 $CLONE $DEPTH https://github.com/tpope/vim-markdown.git
 $CLONE $DEPTH https://github.com/tpope/vim-repeat.git
 $CLONE $DEPTH https://github.com/tpope/vim-surround.git
-$CLONE $DEPTH https://bitbucket.org/telemachus/vim-varia.git
-$CLONE $DEPTH https://github.com/Pychimp/vim-sol.git
-$CLONE $DEPTH https://github.com/dietsche/vim-lastplace.git
