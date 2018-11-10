82704ddb35c003adb6d87e54c84a7f4c377cc965
Peter Aronoff <telemachus@arpinum.org>
2018-11-10 12:34:38 -0500

    Ready for new MBA!

--- home_setup.sh
+++ home_setup.sh
@@ -1,49 +1,43 @@
 #!/usr/bin/env bash
 set -x
 
-# Save this off for later.
-START_DIR=$(pwd)
+START_DIR="$(pwd)"
+BREW_INSTALL="/usr/local/bin/brew install"
 
-# First, let's install Homebrew.
-/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
-/usr/local/bin/brew install git
-/usr/local/bin/brew install dash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+$BREW_INSTALL --with-curl --with-openssl git
+$BREW_INSTALL abook aspell bash bats chgems chruby csvprintf ctags \
+	dash discount ed elinks getmail gist gnupg lua luajit luarocks \
+	mawk mercurial mpack mpc mpd mpack mu ncmpc par pcre2 procmail \
+	pwsafe python remind rename ruby-install urlview wget yank
+$BREW_INSTALL --with-s-lang mutt
+$BREW_INSTALL --HEAD neovim
+$BREW_INSTALL --with-bundle mpv
 
-# Now we need vanilla gcc-4.2 for older rubies
-cd /tmp
-curl -O http://opensource.apple.com/tarballs/gcc/gcc-5666.3.tar.gz
-tar zxf gcc-5666.3.tar.gz
-cd gcc-5666.3
-mkdir -p build/{obj,dst,sym}
-gnumake install RC_OS=macos RC_ARCHS='i386 x86_64' TARGETS='i386 x86_64' \
-SRCROOT=`pwd` OBJROOT=`pwd`/build/obj DSTROOT=`pwd`/build/dst SYMROOT=`pwd`/build/sym
-sudo ditto build/dst /
-cd "$OLDPWD"
-
-if [ ! -d "$HOME/code" ]; then
-    mkdir "$HOME/code"
+if [ ! -d "$HOME/projects" ]; then
+    mkdir "$HOME/projects"
 fi
 
-cd "$HOME/code"
-if [ ! -d "$HOME/code/dotfiles" ]; then
+cd "$HOME/projects"
+if [ ! -d "$HOME/projects/dotfiles" ]; then
     git clone https://github.com/telemachus/dotfiles.git
 else
-    cd "$HOME/code/dotfiles"
+    cd "$HOME/projects/dotfiles"
     git pull origin master
 fi
 cd $HOME
 
 COMMAND='ln -s'
-$COMMAND "$HOME/code/dotfiles/bash_aliases" "$HOME/.bash_aliases"
-$COMMAND "$HOME/code/dotfiles/bash_completion" "$HOME/.bash_completion"
-$COMMAND "$HOME/code/dotfiles/bash_functions" "$HOME/.bash_functions"
-$COMMAND "$HOME/code/dotfiles/bashrc" "$HOME/.bashrc"
-$COMMAND "$HOME/code/dotfiles/gitignore_global" "$HOME/.gitignore_global"
-$COMMAND "$HOME/code/dotfiles/vim/gvimrc" "$HOME/.gvimrc"
-$COMMAND "$HOME/code/dotfiles/inputrc" "$HOME/.inputrc"
-$COMMAND "$HOME/code/dotfiles/irbrc" "$HOME/.irbrc"
-$COMMAND "$HOME/code/dotfiles/procmailrc" "$HOME/.procmailrc"
-$COMMAND "$HOME/code/dotfiles/profile" "$HOME/.profile"
-$COMMAND "$HOME/code/dotfiles/vim/vimrc" "$HOME/.vimrc"
+$COMMAND "$HOME/projects/dotfiles/bash_aliases" "$HOME/.bash_aliases"
+$COMMAND "$HOME/projects/dotfiles/bash_completion" "$HOME/.bash_completion"
+$COMMAND "$HOME/projects/dotfiles/bash_functions" "$HOME/.bash_functions"
+$COMMAND "$HOME/projects/dotfiles/bashrc" "$HOME/.bashrc"
+$COMMAND "$HOME/projects/dotfiles/gitignore_global" "$HOME/.gitignore_global"
+$COMMAND "$HOME/projects/dotfiles/vim/gvimrc" "$HOME/.gvimrc"
+$COMMAND "$HOME/projects/dotfiles/inputrc" "$HOME/.inputrc"
+$COMMAND "$HOME/projects/dotfiles/irbrc" "$HOME/.irbrc"
+$COMMAND "$HOME/projects/dotfiles/procmailrc" "$HOME/.procmailrc"
+$COMMAND "$HOME/projects/dotfiles/profile" "$HOME/.profile"
+$COMMAND "$HOME/projects/dotfiles/vim/vimrc" "$HOME/.vimrc"
 
 cd "$START_DIR"
--- nvim-kickstart.sh
+++ nvim-kickstart.sh
@@ -16,30 +16,29 @@ mkdir $HOME/.nvim_undo
 cp -v ./vim/nvimrc $HOME/.config/init.vim
 
 CLONE='git clone'
-DEPTH='--depth 1'
 
 cd $HOME/.config/nvim/autoload
 curl -S -o pathogen.vim \
     https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd $HOME/.config/nvim/bundle
-$CLONE $DEPTH https://github.com/mattn/gist-vim.git
-$CLONE $DEPTH ttps://github.com/NLKNguyen/papercolor-theme.git
-$CLONE $DEPTH ttps://github.com/nice/sweater.git
-$CLONE $DEPTH ttps://github.com/SirVer/ultisnips.git
-$CLONE $DEPTH ttps://github.com/tpope/vim-abolish.git
-$CLONE $DEPTH ttps://github.com/markcornick/vim-bats.git
-$CLONE $DEPTH ttps://github.com/telemachus/vim-bibcite.git
-$CLONE $DEPTH ttps://github.com/tpope/vim-commentary.git
-$CLONE $DEPTH ttps://github.com/dietsche/vim-lastplace.git
-$CLONE $DEPTH ttps://bitbucket.org/telemachus/vim-macrons.git
-$CLONE $DEPTH ttps://github.com/tpope/vim-markdown.git
-$CLONE $DEPTH ttps://github.com/wlangstroth/vim-racket.git
-$CLONE $DEPTH ttps://github.com/tpope/vim-repeat.git
-$CLONE $DEPTH ttps://github.com/machakann/vim-sandwich.git
-$CLONE $DEPTH ttps://github.com/Pychimp/vim-sol.git
-$CLONE $DEPTH ttps://github.com/tpope/vim-surround.git
-$CLONE $DEPTH ttps://github.com/reedes/vim-textobj-quote.git
-$CLONE $DEPTH ttps://github.com/kana/vim-textobj-user.git
-$CLONE $DEPTH ttps://bitbucket.org/telemachus/vim-varia.git
-$CLONE $DEPTH ttps://github.com/mattn/webapi-vim.git
+$CLONE https://github.com/mattn/gist-vim.git
+$CLONE https://github.com/NLKNguyen/papercolor-theme.git
+$CLONE https://github.com/nice/sweater.git
+$CLONE https://github.com/SirVer/ultisnips.git
+$CLONE https://github.com/tpope/vim-abolish.git
+$CLONE https://github.com/markcornick/vim-bats.git
+$CLONE https://github.com/telemachus/vim-bibcite.git
+$CLONE https://github.com/tpope/vim-commentary.git
+$CLONE https://github.com/dietsche/vim-lastplace.git
+$CLONE https://bitbucket.org/telemachus/vim-macrons.git
+$CLONE https://github.com/tpope/vim-markdown.git
+$CLONE https://github.com/wlangstroth/vim-racket.git
+$CLONE https://github.com/tpope/vim-repeat.git
+$CLONE https://github.com/machakann/vim-sandwich.git
+$CLONE https://github.com/Pychimp/vim-sol.git
+$CLONE https://github.com/tpope/vim-surround.git
+$CLONE https://github.com/reedes/vim-textobj-quote.git
+$CLONE https://github.com/kana/vim-textobj-user.git
+$CLONE https://bitbucket.org/telemachus/vim-varia.git
+$CLONE https://github.com/mattn/webapi-vim.git
