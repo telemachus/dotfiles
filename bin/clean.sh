892fc17fbe48bf21a9a75cdc95c880ca59f8e193
Peter Aronoff <peter@aronoff.org>
2020-05-19 12:45:08 -0400

    Cleaning up

--- bin/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/dash
-
-chmod -fN $HOME/Public
-chmod -fN $HOME/Movies
-chmod -fN $HOME/Music
-rm -rf $HOME/Public
-rm -rf $HOME/Movies
-rm -rf $HOME/Music
--- bin/getmail.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/local/bin/getmail --rcfile getmailrc0 --rcfile getmailrc1 --quiet
