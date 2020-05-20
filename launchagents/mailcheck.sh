f2ef52d060f0a576a0cb4742b8f2315098f7a102
Peter Aronoff <peter@aronoff.org>
2020-05-20 07:29:32 -0400

    Clean up

--- launchagents/mailcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/dash
-
-getmail="/usr/local/bin/getmail"
-
-$getmail --rcfile getmailrc0 --rcfile getmailrc1 --quiet
--- launchagents/mu_index.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/dash
-
-/usr/local/bin/mu index --quiet --maildir /Users/user/.maildir
