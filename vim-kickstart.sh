8310a7182aa7af9c8a1df245484698cdcb6933e9
Peter Aronoff <peter@aronoff.org>
2021-11-07 09:20:29 -0500

    Update vim kickstart

--- vim-kickstart.sh
+++ vim-kickstart.sh
@@ -28,27 +28,22 @@ CLONE="git clone"
 
 cd "$START" || exit
 
-$CLONE https://git.sr.ht/~telemachus/vim-bibcite
 $CLONE https://git.sr.ht/~telemachus/vim-dotfiles
 $CLONE https://git.sr.ht/~telemachus/vim-go-mini
-$CLONE https://git.sr.ht/~telemachus/vim-macrons
 $CLONE https://git.sr.ht/~telemachus/vim-textobj-curly
 $CLONE https://github.com/SirVer/ultisnips.git
+$CLONE https://github.com/benizi/vim-automkdir.git
 $CLONE https://github.com/bps/vim-textobj-python.git
 $CLONE https://github.com/cespare/vim-toml.git
 $CLONE https://github.com/cormacrelf/vim-colors-github.git
 $CLONE https://github.com/dietsche/vim-lastplace.git
 $CLONE https://github.com/inkarkat/SyntaxAttr.vim.git
-$CLONE https://github.com/junegunn/vader.vim
 $CLONE https://github.com/kana/vim-textobj-entire.git
 $CLONE https://github.com/kana/vim-textobj-line.git
 $CLONE https://github.com/kana/vim-textobj-user.git
 $CLONE https://github.com/lambdalisue/suda.vim.git
 $CLONE https://github.com/machakann/vim-sandwich.git
-$CLONE https://github.com/sainnhe/edge.git
 $CLONE https://github.com/tpope/vim-abolish.git
 $CLONE https://github.com/tpope/vim-commentary.git
-$CLONE https://github.com/tpope/vim-dispatch.git
 $CLONE https://github.com/tpope/vim-markdown.git
 $CLONE https://github.com/tpope/vim-repeat.git
-$CLONE https://github.com/vim-scripts/indentpython.vim.git
