cc32273f786a1025351cc6a5628895c200b86256
Peter Aronoff <peter@aronoff.org>
2022-04-02 07:54:13 -0400

    feat(mail): add script to create maildir skeleton

---
+++ mail-skel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -u
+
+for dir in drafts inbox junk recent search sent spam
+do
+	mkdir -v -p "$HOME"/.maildir/"${dir}"/{cur,new,tmp}
+done
+
+YEAR="$(date +%Y)"
+for YYYY in $(seq 2009 $YEAR)
+do
+	mkdir -v -p "$HOME"/.maildir/archive/"${YYYY}"/{cur,new,tmp}
+done
