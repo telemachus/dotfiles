a31d66142db9280742b5071af89e44dd25f09079
Peter Aronoff <peter@aronoff.org>
2022-12-03 15:47:41 -0500

    chore(go-*): install @latest rather than @master

--- go-binaries.bash
+++ go-binaries.bash
@@ -2,12 +2,12 @@
 
 go install github.com/kisielk/errcheck@latest || exit 1
 go install github.com/rogpeppe/godef@latest || exit 1
-go install golang.org/x/tools/cmd/goimports@master || exit 1
+go install golang.org/x/tools/cmd/goimports@latest || exit 1
 go install github.com/mgechev/revive@latest || exit 1
 go install golang.org/x/tools/gopls@latest || exit 1
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest || exit 1
 go install honnef.co/go/tools/cmd/staticcheck@latest || exit 1
-go install golang.org/x/tools/cmd/gorename@master || exit 1
+go install golang.org/x/tools/cmd/gorename@latest || exit 1
 go install github.com/fatih/motion@latest || exit 1
 go install mvdan.cc/gofumpt@latest || exit 1
 go install github.com/segmentio/golines@latest || exit 1
