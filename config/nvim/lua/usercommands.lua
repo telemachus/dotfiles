f2b55489c104f64225b29be1f4ff27459d8a8834
Peter Aronoff <peter@aronoff.org>
2022-12-27 20:16:42 -0500

    fix: update and improve user commands

--- config/nvim/lua/usercommands.lua
+++ config/nvim/lua/usercommands.lua
@@ -2,17 +2,17 @@ local command = vim.api.nvim_create_user_command
 local buf_opt = vim.bo
 local cmd = vim.cmd
 
+command('Gofmt', function(_)
+    require('refinery').format('gofmt', { '-w', '-s' })
+end, { desc = 'Format current file with gofmt', bar = true })
+
+command('Gofumpt', function(_)
+    require('refinery').format('gofumpt', { '-w' })
+end, { desc = 'Format current file with gofumpt', bar = true })
+
 command('Goimports', function(_)
     require('refinery').format('goimports', { '-w' })
 end, { desc = 'Format current file with goimports', bar = true })
-
-command('Gofumpt', function(_)
-    require('refinery').format('gofumpt', { '-w', '-s' })
-end, { desc = 'Format current file with gofumpt', bar = true })
-
-command('Gofmt', function(_)
-    require('refinery').format('gofumpt', { '-w', '-s' })
-end, { desc = 'Format current file with gofmt', bar = true })
 
 command('Golines', function(_)
     require('refinery').format('golines', {
@@ -26,7 +26,7 @@ end, { desc = 'Format current file with golines', bar = true })
 
 command('Revive', function(_)
     local old_make = buf_opt.makeprg
-    buf_opt.makeprg = 'revive -config ~/.revive.toml'
+    buf_opt.makeprg = 'revive -config ~/go/extras/revive.toml'
     cmd('silent make')
     buf_opt.makeprg = old_make
 end, { desc = 'Lint with revive', bar = true })
