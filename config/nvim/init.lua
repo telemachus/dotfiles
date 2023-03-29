adfbddeee310b01886be8f120b5793ee94975768
Peter Aronoff <peter@aronoff.org>
2023-03-29 09:25:35 -0400

    chore: tweak init.lua and cli.txtar

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,8 +14,8 @@ g.localmapleader = ' '
 -- See also this issue re UltiSnips: https://bit.ly/3w1nN9y.
 -- g.python_host_skip_check = 1
 -- g.python3_host_prog = fn.fnamemodify('~/', ':p') .. '.pyenv/shims/python'
-g.loaded_python3_provider = 0
 g.loaded_python_provider = 0
+g.loaded_python3_provider = 0
 g.loaded_node_provider = 0
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
