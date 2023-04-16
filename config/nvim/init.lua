cae9a13f80f7c4377d07d2668e270e5f474e83e3
Peter Aronoff <peter@aronoff.org>
2023-04-16 19:46:15 -0400

    chore: remove setup for Treesitter playground

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -232,24 +232,6 @@ treesitterConfigs.setup({
             },
         },
     },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25,
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<CR>',
-            show_help = '?',
-        },
-    },
 })
 
 local indentBlankline = require('indent_blankline')
