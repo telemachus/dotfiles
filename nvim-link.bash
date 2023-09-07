f4fd244897f2bec4c32e341c41da77bef226bf5f
Peter Aronoff <peter@aronoff.org>
2023-09-06 20:41:53 -0400

    chore: update links (remove grep.lua; add lsp.lua)

--- nvim-link.bash
+++ nvim-link.bash
@@ -6,7 +6,9 @@ BASE="$HOME/.config/nvim"
 
 ln -f -s "$DOTFILES/config/nvim/init.lua" "$BASE/init.lua"
 
-for item in autocommands.lua filetypes.lua grep.lua usercommands.lua mappings.lua;
+for item in autocommands.lua filetypes.lua lsp.lua mappings.lua usercommands.lua;
 do
+	# Create a symbolic link from x to y. If y already exists, delete it
+	# and create a new symbolic link.
 	ln -f -s "$DOTFILES/config/nvim/lua/$item" "$BASE/lua/$item"
 done
