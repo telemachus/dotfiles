4dbe2a5a55cacd0fd604746404b26a98efc071d8
Peter Aronoff <peter@aronoff.org>
2026-01-11 19:29:08 -0500

    Use 4 spaces to show tabs in C files

--- config/nvim/after/ftplugin/c.lua
+++ config/nvim/after/ftplugin/c.lua
@@ -1,8 +1,8 @@
 local bo = vim.bo
 local join = table.concat
 
-bo.tabstop = 8
-bo.softtabstop = 8
-bo.shiftwidth = 8
+bo.tabstop = 4
+bo.softtabstop = 4
+bo.shiftwidth = 4
 bo.expandtab = false
 bo.cinoptions = bo.cinoptions .. join({ "l1", "t0", "+4", "(0", ":0" }, ",")
